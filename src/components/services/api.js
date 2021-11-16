7c410b8a21f3209e1156bb00259e408d2ee88069
ElenaPetrus <leliksmp@ukr.net>
2021-11-16 21:11:51 +0200

    done

--- src/components/services/api.js
+++ src/components/services/api.js
@@ -4,11 +4,10 @@ import axios from 'axios';
 // const language = 'en-US';
 axios.defaults.baseURL = 'https://618a9d5f34b4f400177c47c6.mockapi.io/api/v1';
 
-async function getContacts() {
+export async function getContacts() {
   const response = await axios.get(`/contacts`);
   return await response.data;
 }
-export default getContacts;
 
 export const addContact = async value => {
   const { data } = await axios.post('/contacts', value);
--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -47,30 +47,29 @@
 // export default rootReducer;
 
 import { combineReducers, createReducer } from '@reduxjs/toolkit';
-import {
-  getContacts,
-  addContact,
-  deleteContact,
-  filterContact,
-} from './operations';
+import operations from './operations';
+import { filterContact } from './actions';
 
 const items = createReducer([], {
-  [getContacts.fulfilled]: (_, { payload }) => payload,
-  [addContact.fulfilled]: (state, { payload }) => [...state, payload],
-  [deleteContact.fulfilled]: (state, { payload }) =>
+  [operations.getContacts.fulfilled]: (_, { payload }) => payload,
+  [operations.addContact.fulfilled]: (state, { payload }) => [
+    ...state,
+    payload,
+  ],
+  [operations.deleteContact.fulfilled]: (state, { payload }) =>
     state.filter(({ id }) => id !== payload),
 });
 
 const loading = createReducer(false, {
-  [getContacts.pending]: () => true,
-  [getContacts.fulfilled]: () => false,
-  [getContacts.rejected]: () => false,
-  [addContact.pending]: () => true,
-  [addContact.fulfilled]: () => false,
-  [addContact.rejected]: () => false,
-  [deleteContact.pending]: () => true,
-  [deleteContact.fulfilled]: () => false,
-  [deleteContact.rejected]: () => false,
+  [operations.getContacts.pending]: () => true,
+  [operations.getContacts.fulfilled]: () => false,
+  [operations.getContacts.rejected]: () => false,
+  [operations.addContact.pending]: () => true,
+  [operations.addContact.fulfilled]: () => false,
+  [operations.addContact.rejected]: () => false,
+  [operations.deleteContact.pending]: () => true,
+  [operations.deleteContact.fulfilled]: () => false,
+  [operations.deleteContact.rejected]: () => false,
 });
 
 const filter = createReducer('', {
@@ -78,8 +77,8 @@ const filter = createReducer('', {
 });
 
 const error = createReducer(null, {
-  [getContacts.rejected]: (_, action) => action.payload,
-  [getContacts.pending]: () => null,
+  [operations.getContacts.rejected]: (_, action) => action.payload,
+  [operations.getContacts.pending]: () => null,
 });
 
 const rootReducer = combineReducers({
